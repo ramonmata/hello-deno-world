f074cb181280569ca808dc29793cb0373fa6e058
Ramón Mata <ramonmata@users.noreply.github.com>
2021-06-30 10:57:38 -0500

    Updates Hello Reponse

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 addEventListener("fetch", (event) => {
     event.respondWith(
-        new Response("Hello world", {
+        new Response("Hello Deno World!", {
             status: 200,
             headers: { "content-type": "text/plain" },
         }),
