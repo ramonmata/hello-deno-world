cacbc49b354f734263ff0e2692deec8f35e6aea8
Ramón Mata <ramonmata@users.noreply.github.com>
2021-09-25 20:49:12 -0500

    Adds IP address to message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 addEventListener("fetch", (event) => {
+    const ip = event.request.headers.get("x-forwarded-for");
     event.respondWith(
-        new Response("Hello Deno World!", {
+        new Response(`Hello %{ip} and Welcome to Deno World!`, {
             status: 200,
             headers: { "content-type": "text/plain" },
         }),
